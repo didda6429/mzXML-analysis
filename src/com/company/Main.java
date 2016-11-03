7e3c86dc9f1c767c4fb54bd03d1264e89452d271
didda6429 <adithyadiddapur@gmail.com>
2016-11-03 10:00:34 +0800

    Creates a basic object structure and implements a peak-picking algorithm. See Readme.MD for more details.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,120 @@
 package com.company;
+import umich.ms.datatypes.LCMSDataSubset;
+import umich.ms.datatypes.scan.IScan;
+import umich.ms.datatypes.scan.StorageStrategy;
+import umich.ms.datatypes.scancollection.impl.ScanCollectionDefault;
+import umich.ms.datatypes.spectrum.ISpectrum;
+import umich.ms.fileio.exceptions.FileParsingException;
+import umich.ms.fileio.filetypes.mzxml.MZXMLFile;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.TreeMap;
+
+//"S:\\mzXML Sample Data\\7264381_RP_pos.mzXML"
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-        System.out.println("hello world");
+    public static ArrayList<IScan> scanArrayList;
+    static String location = "S:\\mzXML Sample Data\\7264381_RP_pos.mzXML";
+
+    public static void main(String[] args) throws FileParsingException {
+
+        // Creating data source
+        Path path = Paths.get(location);
+        MZXMLFile source = new MZXMLFile(path.toString());
+
+        // This is a data structure used to store scans and to navigate around the run
+        ScanCollectionDefault scans = new ScanCollectionDefault();
+        // Softly reference spectral data, make it reclaimable by GC
+        scans.setDefaultStorageStrategy(StorageStrategy.SOFT);
+        // Set it to automatically re-parse spectra from the file if spectra were not
+        // yet parsed or were reclaimed to make auto-loading work you'll need to use
+        // IScan#fetchSpectrum() method instead of IScan#getSpectrum()
+        scans.isAutoloadSpectra(true);
+
+        // Set our mzXML file as the data source for this scan collection
+        scans.setDataSource(source);
+        // Set number of threads for multi-threaded parsing.
+        // null means use as many cores as reported by Runtime.getRuntime().availableProcessors()
+        source.setNumThreadsForParsing(null);
+        // load the meta-data about the whole run, with forced parsing of MS1 spectra
+        // as we have enabled auto-loading, then if we ever invoke IScan#fetchSpectrum()
+        // on an MS2 spectrum, for which the spectrum has not been parsed, it will be
+        // obtained from disk automatically. And because of Soft referencing, the GC
+        // will be able to reclaim it.
+        try {
+            scans.loadData(LCMSDataSubset.MS1_WITH_SPECTRA);
+        } catch (FileParsingException e){
+            System.out.println("FileParsingException line 51");
+        }
+        int i=0;
+        // let's traverse the data-structure
+        TreeMap<Integer, IScan> num2scanMap = scans.getMapNum2scan();
+        scanArrayList = new ArrayList<>(); //ArrayList containing only the data from the scans with spectrums
+        for (IScan scan : num2scanMap.values()) {
+            ISpectrum spectrum = scan.getSpectrum();
+            if (spectrum != null) {
+                //System.out.printf("%s does NOT have a parsed spectrum\n", scan.toString());
+                //System.out.printf("%s has a parsed spectrum, it contains %d data points\n",
+                //        scan.toString(), spectrum.getMZs().length);
+                scanArrayList.add(scan);
+                i++;
+            }
+        }
+
+
+        //creates an ArrayList containing only the spectrum data from scanArrayList
+        ArrayList<ISpectrum> spectrumArrayList = new ArrayList<>();
+        for(IScan scan : scanArrayList){
+            spectrumArrayList.add(scan.fetchSpectrum());
+        }
+
+        //finds the maximum intensity peak across the entire database
+        // NOTE --> this is only being used for testing at the moment
+        LocalPeak maxPeak = maxIntensityPeak(spectrumArrayList,400,50);
+
+
+        //LocalPeak maxPeak = Peak.maxIntWithinTol(spectrumArrayList.get(540),616.2,400,540,scanArrayList.get(540).getRt());
+        //LocalPeak maxPeak = Peak.maxIntWithinTol(spectrumArrayList.get(spec1),location,400,spec1,time); //finds the globally maximum peak
+        Peak peak1 = new Peak(scanArrayList, maxPeak, 400, 500);
+        System.out.println(peak1.getIntensityScanPairs().size());
+        System.out.println("test");
+    }
+
+    /**
+     * Function which scans the dataset to find the maximum intensity datapoints. The number of peaks found
+     * is (approximately?) equal to the value of numOfPeaks and then only the single highest value is returned.
+     * @param spectra an ArrayList containing all the spectrum data from all the scans
+     * @param tol The tolerance to jitter (passed to a call to Peak.maxIntWithinTol)
+     * @param numOfPeaks The number of peaks to look for
+     * @return The highest intensity peak across the entire dataset
+     */
+    static LocalPeak maxIntensityPeak(ArrayList<ISpectrum> spectra, double tol, int numOfPeaks){
+        //LinkedList which stores the maximum peaks
+        //In this usage, the LinkedList is treated as a FILO stack
+        LinkedList<LocalPeak> tempList = new LinkedList<>();
+        for(int i=0; i<numOfPeaks; i++){
+            tempList.push(Peak.maxIntWithinTol(spectra.get(1),spectra.get(1).getMZs()[0],tol,1,scanArrayList.get(1).getRt()));
+        }
+        //loops which find the maximum intensities and add them to the stack (LinkedList)
+        for(int i = 0; i<spectra.size(); i++){
+            double maxV = spectra.get(i).getMaxInt();
+            double loc = spectra.get(i).getMaxIntMz();
+            for(int j=0; j<numOfPeaks-1; j++) {
+                if (maxV > tempList.get(j).getIntensity()) {
+                    tempList.add(j,Peak.maxIntWithinTol(spectra.get(i), loc, tol, i, scanArrayList.get(i).getRt()));
+                    j=11;
+                }
+            }
+        }
+        //return Peak.maxIntWithinTol(spectra.get(spec1),location,tol,spec1,time);
+        //for(int i=tempList.size(); i>tempList.size()-10; i--){
+        //    tempList.remove(i-1);
+        //}
+        return tempList.pop();
     }
 }
---
+++ src/com/company/Peak.java
@@ -0,0 +1,206 @@
+package com.company;
+
+import umich.ms.datatypes.scan.IScan;
+import umich.ms.datatypes.spectrum.ISpectrum;
+import umich.ms.fileio.exceptions.FileParsingException;
+
+import java.util.ArrayList;
+
+/**
+ * Represents a group of LocalPeaks which together form a peak which is significant across the entire dataset.
+ * (This is the peak which will later be integrated and analysed)
+ *
+ * All of the class member fields are private to help ensure reproducibility (They can't be changed which ensures
+ * that you can read them later to find the same values which were used to create the object)
+ *
+ * @author Adithya Diddapur
+ */
+public class Peak {
+
+    private ArrayList<LocalPeak> intensityScanPairs;
+    private ArrayList<LocalPeak> intensityScanPairsAbove;
+    private ArrayList<LocalPeak> intensityScanPairsBelow;
+    private double meanMZ;
+    private double tolerance;
+    private double threshold; //used to define noise to signal ratio
+    private int startingPointIndex; //index of the max peak within the ArrayList (max intensity)
+
+    /**
+     * Constructor which creates a new Peak. This class is designed so that in normal use, a user only every needs
+     * to call the constructor which acts as a wrapper for everything.
+     * i.e. Once called, the constructor initialises all the relevant variables and runs the recursive algorithm to
+     * find the edges of the peak (where it stops being significant) Note: at the moment significance is determined by
+     * a constant (thresh).
+     *
+     * @param scanList An ArrayList of all the scans (as IScan objects)
+     * @param startingPoint The LocalPeak to use as the starting point for the larger peak
+     * @param tol The tolerance (in ppm) to account for the jitter
+     * @param thresh The threshold to determine the end points of the peak
+     * @throws FileParsingException Thrown when the recursive loops try to access the scan data
+     */
+    public Peak(ArrayList<IScan> scanList, LocalPeak startingPoint, double tol, double thresh) throws FileParsingException {
+        intensityScanPairs = new ArrayList<>();
+        intensityScanPairsAbove = new ArrayList<>();
+        intensityScanPairsAbove.add(startingPoint);
+        intensityScanPairsBelow = new ArrayList<>();
+        tolerance = tol;
+        threshold = thresh;
+        meanMZ = startingPoint.getMZ();
+        //System.out.println(createPeakAbove(scanList, averageMZ(), 400, startingPoint.getScanNumber() + 1));
+        System.out.println(createPeakBelow(scanList, meanMZ, 400, startingPoint.getScanNumber() - 1));
+        for (int i = intensityScanPairsBelow.size(); i > 0; i--) {
+            intensityScanPairs.add(intensityScanPairsBelow.get(i - 1));
+        }
+        intensityScanPairs.add(startingPoint);
+        System.out.println(createPeakAbove(scanList, averageMZ(), 400, startingPoint.getScanNumber() + 1));
+        startingPointIndex = intensityScanPairsBelow.size();
+        System.out.println("test");
+    }
+
+    /**
+     * Recursive loop which 'looks' at the scans above the starting points (higher RT) to find where the peak ends.
+     * @param scanList An ArrayList of all the scans (as IScan objects)
+     * @param average The average m/z value of the peak. This is used to account for the jitter and is re-calculated
+     *                once for each recursive iteration.
+     * @param toler The tolerance (in ppm) to account for the jitter
+     * @param increment Used to iterate through one of the loops is a semi-recursive manner
+     * @return the integer 1 if the operation was carried out successfully
+     * @throws FileParsingException Thrown when the recursive loops try to access the scan data
+     */
+    private int createPeakAbove(ArrayList<IScan> scanList, double average, double toler, int increment) throws FileParsingException {
+        ISpectrum temp = scanList.get(increment).fetchSpectrum();
+        LocalPeak tempPeak = maxIntWithinTol(temp, average, toler, increment, scanList.get(increment).getRt());
+        if (tempPeak.getIntensity() > threshold) {
+            intensityScanPairs.add(tempPeak);
+            System.out.println(increment);
+            return createPeakAbove(scanList, averageMZ(), toler, increment + 1);
+        } else {
+            return 1;
+        }
+    }
+
+    /**
+     * Recursive loop which 'looks' at the scans below the starting points (lower RT) to find where the peak ends.
+     * @param scanList An ArrayList of all the scans (as IScan objects)
+     * @param average The average m/z value of the peak. This is used to account for the jitter and is re-calculated
+     *                once for each recursive iteration.
+     * @param toler The tolerance (in ppm) to account for the jitter
+     * @param increment Used to iterate through one of the loops is a semi-recursive manner
+     * @return the integer 1 if the operation was carried out successfully
+     * @throws FileParsingException Thrown when the recursive loops try to access the scan data
+     */
+    private int createPeakBelow(ArrayList<IScan> scanList,double average, double toler, int increment) throws FileParsingException {
+        ISpectrum temp = scanList.get(increment).fetchSpectrum();
+        LocalPeak tempPeak = maxIntWithinTol(temp, average, toler, increment, scanList.get(increment).getRt());
+        if (tempPeak.getIntensity() > threshold) {
+            intensityScanPairsBelow.add(tempPeak);
+            System.out.println(increment);
+            return createPeakBelow(scanList, averageMZBelow(), toler, increment - 1);
+        } else {
+            return 1;
+        }
+    }
+
+    /**
+     * Finds the highest single peak within a given tolerance in a individual spectrum(to account for jitter).
+     *
+     * NOTE: This function is crucial to the operation of the Peak data structure and is
+     * called by several other functions at higher levels so be careful when modifying it
+     * @param spec The single spectrum from which to extract a single Peak
+     * @param mean The value around which the tolerance is centered (this partly defines where the single peak will
+     *             be extracted from
+     * @param tol The tolerance to jitter
+     * @param increment The scan number (from the parent IScan object) which is then used to create a LocalPeak object
+     *                  containing the relevant information
+     * @param RT The RT of the scan which is used to create a LocalPeak object containing the relevant information
+     * @return A LocalPeak object containing all of the relvant information
+     */
+    static LocalPeak maxIntWithinTol(ISpectrum spec, double mean, double tol, int increment, double RT) {
+        int[] temp = spec.findMzIdxsWithinPpm(mean, tol);   //according to source code tolerance is calculated as (mean/1e6)*tol
+        double[] inten = spec.getIntensities();
+        int maxIndex = 0;
+        double maxIntensity = 0;
+        for (int i = temp[0]; i <= temp[1]; i++) {
+            if (inten[i] > maxIntensity) {
+                maxIntensity = inten[i];
+                maxIndex = i;
+            }
+        }
+        return new LocalPeak(increment, maxIntensity, spec.getMZs()[maxIndex], RT);
+    }
+
+    /**
+     * Calculates the average m/z value of all the LocalPeak objects in the intensityScanPairs ArrayList. This method
+     * returns the value as a double for use in the recursive loops and saves the value to the class field meanMZ
+     * @return The average m/z value as a double
+     */
+    private double averageMZ() {
+        int i = 0;
+        double total = 0;
+        for (LocalPeak intensityScanPair : intensityScanPairs) {
+            total = total + intensityScanPair.getMZ();
+            i++;
+        }
+        double average = total / i;
+        meanMZ = average;
+        return average;
+    }
+
+    /**
+     * The average m/z value of all the LocalPeak objects in the intensityScanPairsBelow ArrayList. Note: This method
+     * is very similar to the averageMZ() method except that it iis specifically intended for use in the
+     * createPeakBelow() method. The calculated value also gets saved to the class field meanMZ
+     * @return the average m/z value from intensityScanPairsBelow
+     */
+    private double averageMZBelow() {
+        int i = 0;
+        double total = 0;
+        for (LocalPeak anIntensityScanPairsBelow : intensityScanPairsBelow) {
+            total = total + anIntensityScanPairsBelow.getMZ();
+            i++;
+        }
+        double average = total / i;
+        meanMZ = average;
+        return average;
+    }
+
+    /**
+     * Returns the intensityScanPairs ArrayList
+     * @return An ArrayList containing the LocalPeak objects from intensityScanPairs
+     */
+    ArrayList<LocalPeak> getIntensityScanPairs(){
+        return intensityScanPairs;
+    }
+
+    /**
+     * Returns the mean m/z for the Peak
+     * @return the mean m/z value as a double
+     */
+    double getMeanMZ(){
+        return meanMZ;
+    }
+
+    /**
+     * Returns the tolerance used to create the peak
+     * @return the tolerance as a double
+     */
+    double getTolerance(){
+        return tolerance;
+    }
+
+    /**
+     * Returns the threshold used to generate the peak
+     * @return The threshold as a double
+     */
+    double getThreshold(){
+        return threshold;
+    }
+
+    /**
+     * Returns the index of the LocalPeak which was used as a starting point
+     * @return the index of the starting point as a integer
+     */
+    int getStartingPointIndex(){
+        return startingPointIndex;
+    }
+}
